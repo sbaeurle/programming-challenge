fd5ff1a65be96c05d55a52470a0bd2689cc991fe
Simon Bäurle <simonbaeurle@outlook.de>
2025-02-28 00:09:48 +0100

    split into subcommands

--- Adapters.CLI/Program.cs
+++ Adapters.CLI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Parsing;
+using System.Globalization;
 using System.IO.Abstractions;
 using BXCP.ProgrammingChallenge.Adapters.Cli;
 using BXCP.ProgrammingChallenge.Adapters.Csv;
@@ -14,18 +15,18 @@ var rootCommand = new RootCommand(
 
 var fileSystem = new FileSystem();
 
+var weatherCommand = new Command("weather", "calculate temperature spread");
+
 var weatherSourceOption = new Option<string>(
     "--weather-source",
-    "Specify the weather data source in the format protocol://source");
+    "Specify the weather data source in the format protocol://source")
+{
+  IsRequired = true
+};
 weatherSourceOption.AddValidator(Validator.ValidateArgument);
 
-var countrySourceOption = new Option<string>(
-    "--country-source",
-    "Specify the country data source in the format protocol://source");
-countrySourceOption.AddValidator(Validator.ValidateArgument);
-
-rootCommand.Add(weatherSourceOption);
-rootCommand.SetHandler(weatherSource =>
+weatherCommand.AddOption(weatherSourceOption);
+weatherCommand.SetHandler(weatherSource =>
 {
   var readers = new Dictionary<string, IWeatherReader>{
     {".csv", new CsvWeatherReader(fileSystem)}
@@ -41,10 +42,43 @@ rootCommand.SetHandler(weatherSource =>
     return;
   }
 
-  var value = result.Value;
-  Console.WriteLine($"Calculated day {value.Day} with minimum temperature spread of {value.TemperatureSpread} from source {weatherSource}");
-
-
+  var weatherRecord = result.Value;
+  Console.WriteLine($"Calculated day {weatherRecord.Day} with minimum temperature spread of {weatherRecord.TemperatureSpread} from source {weatherSource}");
 }, weatherSourceOption);
+
+rootCommand.AddCommand(weatherCommand);
+
+var countryCommand = new Command("country", "calculate population density");
+
+var countrySourceOption = new Option<string>(
+    "--country-source",
+    "Specify the country data source in the format protocol://source")
+{
+  IsRequired = true
+};
+countrySourceOption.AddValidator(Validator.ValidateArgument);
+
+countryCommand.AddOption(countrySourceOption);
+countryCommand.SetHandler(countrySource =>
+{
+  var readers = new Dictionary<string, ICountryReader>{
+    {".csv", new CsvCountryReader(fileSystem)}
+  };
+
+  ICountryService countryService = new CountryService(readers);
+
+  var result = countryService.GetCountryWithHighestPopulationDensity(countrySource);
+
+  if (result.IsFailed)
+  {
+    Console.WriteLine($"Could not calculate country with highest population density: {result}");
+    return;
+  }
+
+  var country = result.Value;
+  Console.WriteLine($"Calculated country {country.Name} with highest population density of {country.PopulationDensity.ToString("F2", CultureInfo.CurrentCulture)} per km² from source {countrySource}");
+}, countrySourceOption);
+
+rootCommand.AddCommand(countryCommand);
 
 return await rootCommand.InvokeAsync(args);
